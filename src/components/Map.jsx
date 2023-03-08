08cd31e534ce74a95201ac037e10f4018f1845d5
Volodymyr <vvolodya1110@gmail.com>
2023-03-08 12:37:10 +0200

    add other sections

---
+++ src/components/Map.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {ComposableMap, Geographies, Geography, Annotation} from "react-simple-maps";
+
+const Map = () => {
+  return (
+    <ComposableMap
+      projection="geoAzimuthalEqualArea"
+      projectionConfig={{
+        rotate: [-10.0, -52.0, 0],
+        center: [0, -3],
+        scale: 1000
+      }}
+      style={{width:"100%", height:"100%"}}
+    >
+      <Geographies
+        geography="/features.json"
+        fill="#2C065D"
+        stroke="#FFFFFF"
+        strokeWidth={0.5}
+      >
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+      <Annotation
+        subject={[24.1 , 49.9]}
+        dx={-90}
+        dy={-30}
+        connectorProps={{
+          stroke: "white",
+          strokeWidth: 2,
+          strokeLinecap: "round"
+        }}
+      >
+        <text x="-15" fontSize={'24px'} textAnchor="end" alignmentBaseline="middle" fill="white">
+          {"We are in Lviv"}
+        </text>
+      </Annotation>
+    </ComposableMap>
+  );
+};
+
+export default Map;
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,18 +1,89 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const data =[
+  "Web Design" ,
+  "Developvent",
+  "Illustrator",
+  "Product Design",
+  "Social Media"
+]
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+`
 
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+`
+
+const ListItem = styled.li`
+  font-size: 90px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+
+  ::after{
+    content: "${(props)=>props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    color: pink;
+    width: 0;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  &:hover{
+    ::after{
+      animation: movetext 0.5s linear both;
+      @keyframes movetext {
+        to{
+          width: 100%;
+        }
+      }
+    }
+  }
+`
+
+const Right = styled.div`
+  flex: 1;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  
 `
 
 
 const Works = () => {
   return (
     <Section>
-      
+      <Container>
+        <Left>
+          <List>
+            {data.map(i=>(<ListItem key={i} text={i}>{i}</ListItem>))}
+          </List>
+        </Left>
+        <Right>
+
+        </Right>
+      </Container>
     </Section>
   )
 }
